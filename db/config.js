c2966268fce65210e466d322e3a9daba638ff7b0
IbrahimAbotteen <hemaabotteen@gmail.com>
2020-09-20 22:31:56 -0700

    create react,+ config file

---
+++ db/config.js
@@ -0,0 +1,21 @@
+const options = {
+    query: (e) => {
+      console.log(e.query);
+    }
+  }
+  const pgp = require('pg-promise')(options);
+
+  function setDatabase() {
+    if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+        return pgp({
+            database: 'schoolizer',
+            port: 5432,
+            host: 'localhost',
+            user:process.env.NAME,
+            password:process.env.PASSWORD
+        });
+    } else if (process.env.NODE_ENV === 'production') {
+        return pgp(process.env.DATABASE_URL);
+    };
+  };
+  module.exports = setDatabase();
