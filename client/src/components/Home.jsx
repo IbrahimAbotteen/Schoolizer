c54e5f97a1c6a19a10d0da66623b2122c9adb2fc
IbrahimAbotteen <hemaabotteen@gmail.com>
2020-09-22 09:18:10 -0700

    React Setup

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {link} from 'react-router-dom'
+
+const Home=()=>{
+    return (
+        <div>
+            <div>
+                <button>Students</button>
+            </div>
+            <div>
+                <button>Teachers</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
