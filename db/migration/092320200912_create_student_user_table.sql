7dfa39c107f4341f092c0cb853e3d651eef56e6f
IbrahimAbotteen <hemaabotteen@gmail.com>
2020-09-23 19:02:04 -0700

    adding new col to the users table

--- db/migration/092320200912_create_student_user_table.sql
+++ db/migration/092320200912_create_student_user_table.sql
@@ -2,5 +2,9 @@ CREATE TABLE IF NOT EXISTS student_user (
   id SERIAL PRIMARY KEY,
   username VARCHAR(255) UNIQUE NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
-  password_digest TEXT NOT NULL
+  password_digest TEXT NOT NULL,
+
 );
+
+Alter table student_user
+ADD COLUMN access VARCHAR NOT NULL;
