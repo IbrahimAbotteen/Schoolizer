6b7868f47712084dd5079dffeb8441efe53c8121
IbrahimAbotteen <hemaabotteen@gmail.com>
2020-09-26 17:49:05 -0700

    creating new Routes for the students

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Home from './components/Home';
 import Header from './components/Header';
 import TeacherController from './components/Teachers/TeacherController'
 import TeachersHomePage from './components/Teachers/TeachersHomePage'
+import StudentController from './components/Students/StudentsHomePage'
+import StudentsHomePage from './components/Students/StudentsHomePage'
 
  class App extends Component {
    constructor(){
@@ -23,24 +25,46 @@ import TeachersHomePage from './components/Teachers/TeachersHomePage'
             <Header/>
             <Route exact path='/' component={Home}/>
 
-            <Route exact path='/teachersHomePage' component={TeachersHomePage} />
+              <Route exact path='/teachersHomePage' component={TeachersHomePage} />
 
-            <Route exact path="/teachers"
-              render={() => <TeacherController currentPage="index" />}
-          />
-
-            <Route exact path="/teachers/:id"
-              render={props =>( <TeacherController currentPage="show" currentId={props.match.params.id}/>)}
+              <Route exact path="/teachers"
+                render={() => <TeacherController currentPage="index" />}
             />
 
-            <Route exact path='/newTeacher'
-              render={()=>(<TeacherController currentPage='new'/>)}            
-            />
+              <Route exact path="/teachers/:id"
+                render={props =>( <TeacherController currentPage="show" currentId={props.match.params.id}/>)}
+              />
+
+              <Route exact path='/newTeacher'
+                render={()=>(<TeacherController currentPage='new'/>)}            
+              />
 
             <Route exact path="/editTeacher/:id"
               render={props => (<TeacherController
                 currentPage="edit" currentId={props.match.params.id} />)}
             />
+
+          
+            <Route exact path='/studentsHomePage' component={StudentsHomePage} />
+
+            <Route exact path="/students"
+              render={() => <StudentController currentPage="index" />}
+            />
+
+            <Route exact path="/students/:id"
+              render={props =>( <StudentController currentPage="show" currentId={props.match.params.id}/>)}
+            />
+
+            <Route exact path='/newStudent'
+              render={()=>(<StudentController currentPage='new'/>)}            
+            />
+
+            <Route exact path="/editStudent/:id"
+              render={props => (<StudentController
+                currentPage="edit" currentId={props.match.params.id} />)}
+            />
+
+
 
             </div>
           </Router>
