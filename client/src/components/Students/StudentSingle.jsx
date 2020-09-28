39b1ab5f37f266012485ff588dd8ed9f9e3b0bf2
IbrahimAbotteen <hemaabotteen@gmail.com>
2020-09-27 23:22:05 -0700

    styling (Show) single pages

--- client/src/components/Students/StudentSingle.jsx
+++ client/src/components/Students/StudentSingle.jsx
@@ -5,9 +5,12 @@ const StudentSingle=(props)=>{
 
     return(
         <div className='studentInfoContainer'>
-            <h1>Student First Name: {props.student.first_name} </h1>
-            <h1>Student's Last Name: {props.student.last_name}</h1>
-            <h1>Grade Level: {props.student.grade_level}</h1>
+            <div className='studentDescription'>
+                <h1>Student First Name: {props.student.first_name} </h1>
+                <h1>Student's Last Name: {props.student.last_name}</h1>
+                <h1>Grade Level: {props.student.grade_level}</h1>
+            </div>
+
             <h2 className='click1'><Link to={`/editStudent/${props.student.id}`}>Edit Student</Link></h2>
             <h2 className='click2'><Link to={`/students`}>Back</Link></h2>
 
--- client/src/components/Teachers/TeacherSingle.jsx
+++ client/src/components/Teachers/TeacherSingle.jsx
@@ -4,9 +4,11 @@ import {Link,Redirect} from 'react-router-dom';
 const TeacherSingle=(props)=>{
     return(
         <div className='teacherInfoContainer'>
-            <h1>Teachers First Name: {props.teacher.first_name} </h1>
-            <h1>Teacher's Last Name: {props.teacher.last_name}</h1>
-            <h1>Major: {props.teacher.major}</h1>
+            <div className='teacherDescription'>
+                <h1>Teachers First Name: {props.teacher.first_name} </h1>
+                <h1>Teacher's Last Name: {props.teacher.last_name}</h1>
+                <h1>Major: {props.teacher.major}</h1>
+            </div>
             <h2 className='click1'><Link to={`/editTeacher/${props.teacher.id}`}>Edit Teacher</Link></h2>
             <h2 className='click2'><Link to={`/teachers`}>Back</Link></h2>
             {/* <button className="click1" onClick={() => props.teacherDelete(props.teacher.id)}>Delete</button> */}
